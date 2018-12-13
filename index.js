ded1a95c8eeb5f14549d1663143dd5a05a6bc725
dani <mayor@dani.town>
2018-12-12 20:35:43 -0500

    scroll up arrow

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-$(window).on('scroll', function(){
-	var scrollAmount = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  var halfHeight = $(window).height() / 2;
-  var topOfHiddenDiv = $('#back-to-top-button').offset().top;
-	if((scrollAmount > (windowHeight * 2)) && !$('#back-to-top-button').hasClass('show')) {
-    $('#back-to-top-button').addClass('show');
-  }
-});
+// $(window).on('scroll', function(){
+// 	var scrollAmount = $(window).scrollTop();
+//   var windowHeight = $(window).height();
+//   var halfHeight = $(window).height() / 2;
+//   var topOfHiddenDiv = $('#back-to-top-button').offset().top;
+// 	if((scrollAmount > (windowHeight * 2)) && !$('#back-to-top-button').hasClass('show')) {
+//     $('#back-to-top-button').addClass('show');
+//   }
+//   if (scrollAmount < windowHeight * 2) {
+//     $('#back-to-top-button').removeClass('show');
+//   }
+// });
